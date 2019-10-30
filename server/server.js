b3a7346bb603411ee51135fb5a493e6fc6be6d4b
eric <eric@pql.local>
2019-10-30 21:33:56 +0800

    服务端渲染基础配置

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const express = require('express');
 const ReactSSR = require('react-dom/server');
-const path = require('path');
 const serverEntry = require('../dist/server-entry').default;
-
+const path = require('path');
 const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
 
 const app = express();
@@ -13,8 +12,8 @@ app.use('/public', express.static(path.join(__dirname, '../dist')))
 app.get('*', function(req, res) {
     const appString = ReactSSR.renderToString(serverEntry);
     res.send(template.replace('<app></app>', appString));
-});
+})
 
-app.listen(3333, function(req, res) {
+app.listen(3333, function() {
     console.log('server is listening on 3333');
-});
+})
